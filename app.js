c72d2dfdadaf0bb53a336b25df0cd19cf0416f0e
Manu <manupal8954@gmail.com>
2025-04-15 12:55:54 +0530

    Fix Vercel deployment configuration for static files

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const path = require('path')
 
 // Configure Express
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Serve static files with explicit path
+app.use('/css', express.static(path.join(__dirname, 'Public/css')))
+app.use('/js', express.static(path.join(__dirname, 'Public/js')))
+app.use('/img', express.static(path.join(__dirname, 'Public/img')))
 app.use(express.static(path.join(__dirname, 'Public')))
 
 // Define routes
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+// Handle 404
+app.use((req, res) => {
+    res.status(404).send('Page not found')
 })
 
 // Start server in development environment
@@ -21,4 +32,5 @@ if (process.env.NODE_ENV !== 'production') {
 
 // Export for serverless use
 module.exports = app
+
 
