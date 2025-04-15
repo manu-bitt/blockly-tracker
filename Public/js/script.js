6a0e671460d61e5cf07b9548a6e1448107a454c3
Manu <manupal8954@gmail.com>
2025-04-15 13:10:28 +0530

    Update satellite layer attribution to 'Manu----Tracker'

--- Public/js/script.js
+++ Public/js/script.js
@@ -8,7 +8,7 @@ const streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 }).addTo(map);
 
 const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    attribution: 'Manu----Tracker',
     maxZoom: 19
 });
 
